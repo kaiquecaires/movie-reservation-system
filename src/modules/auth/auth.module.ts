41f363d11120692330c32ab7c07e347b4687593c
kaique <caires.kaique@gmail.com>
2024-10-23 06:30:29 -0300

    feat: added users repository to signup service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AuthController } from './auth.controller'
 import { SignUpService } from './services/sign-up.service'
+import { PrismaModule } from 'src/infra/postgresql/prisma.module'
 
 @Module({
   providers: [
     SignUpService
   ],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [PrismaModule]
 })
 export class AuthModule {}
--- src/modules/auth/services/sign-up.service.ts
+++ src/modules/auth/services/sign-up.service.ts
@@ -1,10 +1,23 @@
 import { plainToInstance } from "class-transformer";
 import { SignUpBodyDTO } from "../dto/body/sign-up.dto";
 import { SignUpResponseDTO } from "../dto/responses/sign-up-response.dto";
+import { Injectable } from "@nestjs/common";
+import { UsersRepository } from "src/infra/postgresql/repositories/users.repository";
 
+@Injectable()
 export class SignUpService {
-  async execute({ name, email, password }: SignUpBodyDTO): Promise<SignUpResponseDTO > {
-    console.log(password)
-    return plainToInstance(SignUpResponseDTO, { id: 1, name, email })
+  constructor(
+    private readonly usersRepository: UsersRepository
+  ) {}
+
+  async execute({ name, email, password }: SignUpBodyDTO): Promise<SignUpResponseDTO> {
+    const user = this.usersRepository.create({
+      name,
+      email,
+      password
+    })
+    return plainToInstance(SignUpResponseDTO, user, {
+      excludeExtraneousValues: true
+    })
   }
 }
