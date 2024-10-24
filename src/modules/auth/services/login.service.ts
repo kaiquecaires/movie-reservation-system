0b2adb7318bc7bb1a98ecbd2d3acda3450a2dfaf
kaique <caires.kaique@gmail.com>
2024-10-24 07:07:42 -0300

    feat: added login service

---
+++ src/modules/auth/services/login.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
+import { BcryptService } from './bcrypt.service';
+import { UsersRepository } from 'src/infra/postgresql/repositories/users.repository';
+import { LoginBodyDTO } from '../dto/body/login.dto';
+import { AuthResponseDTO } from '../dto/responses/auth.dto'
+import { JwtService } from './jwt.service';
+
+@Injectable()
+export class LoginService {
+  constructor(
+    private readonly bcryptService: BcryptService,
+    private readonly usersRepository: UsersRepository,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async execute(body: LoginBodyDTO): Promise<AuthResponseDTO> {
+    const user = await this.usersRepository.getByEmail(body.email)
+    if (!user) {
+      throw new NotFoundException('User not found!')
+    }
+
+    const isPasswordCorrect = await this.bcryptService.comparePassword(body.password, user.password)
+
+    if (!isPasswordCorrect) {
+      throw new BadRequestException('Incorrect password!')
+    }
+
+    return {
+      token: this.jwtService.generateToken(user.id)
+    }
+  }
+}
