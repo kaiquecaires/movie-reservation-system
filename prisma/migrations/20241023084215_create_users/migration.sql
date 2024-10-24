f1cd5a42fbfbbcecf6e3f9b594f97119adb6c52d
kaique <caires.kaique@gmail.com>
2024-10-24 06:35:18 -0300

    feat: hash password before saving

--- prisma/migrations/20241023084215_create_users/migration.sql
+++ prisma/migrations/20241023084215_create_users/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "email" TEXT NOT NULL,
-    "password" VARCHAR(12) NOT NULL,
+    "password" VARCHAR(60) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
--- src/modules/auth/services/sign-up.service.ts
+++ src/modules/auth/services/sign-up.service.ts
@@ -3,18 +3,21 @@ import { SignUpBodyDTO } from "../dto/body/sign-up.dto";
 import { SignUpResponseDTO } from "../dto/responses/sign-up-response.dto";
 import { Injectable } from "@nestjs/common";
 import { UsersRepository } from "src/infra/postgresql/repositories/users.repository";
+import { BcryptService } from "./bcrypt.service";
 
 @Injectable()
 export class SignUpService {
   constructor(
-    private readonly usersRepository: UsersRepository
+    private readonly usersRepository: UsersRepository,
+    private readonly bcryptService: BcryptService
   ) {}
 
   async execute({ name, email, password }: SignUpBodyDTO): Promise<SignUpResponseDTO> {
+    const hashedPassword = await this.bcryptService.hashPassword(password)
     const user = this.usersRepository.create({
       name,
       email,
-      password
+      password: hashedPassword
     })
     return plainToInstance(SignUpResponseDTO, user, {
       excludeExtraneousValues: true
