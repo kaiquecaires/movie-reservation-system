84bd6b5a4d393474d3c8c898f5578d140c25a8f1
kaiquecaires <caires.kaique@gmail.com>
2024-10-26 20:12:48 -0300

    feat: added movie controller

---
+++ src/modules/movie/movie.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, HttpCode, Post, Res } from "@nestjs/common";
+import { ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { CreateMovieDTO } from "./dto/body/create-movie.dto";
+import { FastifyReply } from "fastify";
+import { CreateMovieService } from "./services/create-movie.service";
+import { MovieDTO } from "./dto/responses/movie.dto";
+
+@ApiTags('Movies | V1')
+@Controller({
+  version: '1',
+  path: 'movie'
+})
+export class MovieController {
+  constructor(
+    private readonly createMovieService: CreateMovieService
+  ) {}
+
+  @HttpCode(201)
+  @ApiOperation({
+    summary: 'Route to create a movie'
+  })
+  @ApiCreatedResponse({
+    description: 'Result of this route',
+    type: MovieDTO
+  })
+  @Post('/')
+  async create(@Body() body: CreateMovieDTO, @Res() res: FastifyReply) {
+    const result = await this.createMovieService.execute(body)
+    return res.status(201).send(result)
+  }
+
+}
