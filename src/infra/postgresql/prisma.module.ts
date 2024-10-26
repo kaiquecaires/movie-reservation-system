7fd4291cf0b49d83ae77adad6dbe21f626dd0afa
kaiquecaires <caires.kaique@gmail.com>
2024-10-26 20:11:59 -0300

    feat: updated prisma module

--- src/infra/postgresql/prisma.module.ts
+++ src/infra/postgresql/prisma.module.ts
@@ -1,15 +1,18 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 import { UsersRepository } from "./repositories/users.repository";
+import { MoviesRepository } from "./repositories/movies.repository";
 
 @Module({
   providers: [
     PrismaService,
-    UsersRepository
+    UsersRepository,
+    MoviesRepository
   ],
   exports: [
     PrismaService,
-    UsersRepository
+    UsersRepository,
+    MoviesRepository
   ]
 })
 
