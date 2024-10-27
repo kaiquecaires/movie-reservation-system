8e81b49fbf89154848acbbd7f546604db7d572b2
kaiquecaires <caires.kaique@gmail.com>
2024-10-27 17:45:55 -0300

    refactor: changed auth payload from userId to role

--- src/modules/auth/services/jwt.service.ts
+++ src/modules/auth/services/jwt.service.ts
@@ -7,7 +7,7 @@ export class JwtService {
   private readonly expiration = process.env.EXPIRATION_TIME
 
   generateToken(userId: number, role: string): string {
-    return jwt.sign({ userId, role }, this.secret, { expiresIn: this.expiration })
+    return jwt.sign({ sub: userId, role }, this.secret, { expiresIn: this.expiration })
   }
 
   verifyToken(token: string): any {
