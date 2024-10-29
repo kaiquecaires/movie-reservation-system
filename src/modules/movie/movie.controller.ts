9e61f6e2c6651ee017f17878c22372df4046bd96
kaiquecaires <caires.kaique@gmail.com>
2024-10-29 06:21:29 -0300

    feat: added update movie route

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, HttpCode, Post, Req, Res, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
-import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, HttpCode, Param, ParseIntPipe, Post, Put, Req, Res, UseGuards, UseInterceptors } from "@nestjs/common";
+import { ApiBearerAuth, ApiCreatedResponse, ApiNoContentResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { CreateMovieDTO } from "./dto/body/create-movie.dto";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { CreateMovieService } from "./services/create-movie.service";
@@ -7,6 +7,8 @@ import { MovieDTO } from "./dto/responses/movie.dto";
 import { AuthGuard } from "@nestjs/passport";
 import { FastifyFileInterceptor } from "src/interceptors/fastify-file.interceptor";
 import { UpdloadPosterImageDTO } from "./dto/responses/upload-poster-image.dto";
+import { UpdateMovieDTO } from "./dto/body/update-movie.dto";
+import { UpdateMovieService } from "./services/update-movie.service";
 
 @ApiTags('Movies | V1')
 @Controller({
@@ -15,7 +17,8 @@ import { UpdloadPosterImageDTO } from "./dto/responses/upload-poster-image.dto";
 })
 export class MovieController {
   constructor(
-    private readonly createMovieService: CreateMovieService
+    private readonly createMovieService: CreateMovieService,
+    private readonly updateMovieService: UpdateMovieService
   ) {}
 
   @HttpCode(201)
@@ -49,5 +52,21 @@ export class MovieController {
   async uploadPosterImage(@Req() request: FastifyRequest) {
     const filePath = (request as any).filePath;
     return { path: filePath }
+  }
+
+  @HttpCode(204)
+  @ApiOperation({
+    summary: 'Route to update movie'
+  })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @ApiNoContentResponse()
+  @Put('/:id')
+  async updateMovie(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateMovieDTO, @Res() res: FastifyReply
+  ) {
+    await this.updateMovieService.execute(id, body)
+    return res.status(204).send()
   }
 }
--- src/modules/movie/movie.module.ts
+++ src/modules/movie/movie.module.ts
@@ -2,10 +2,12 @@ import { PrismaModule } from "src/infra/postgresql/prisma.module";
 import { MovieController } from "./movie.controller";
 import { CreateMovieService } from "./services/create-movie.service";
 import { Module } from "@nestjs/common";
+import { UpdateMovieService } from "./services/update-movie.service";
 
 @Module({
   providers: [
-    CreateMovieService
+    CreateMovieService,
+    UpdateMovieService
   ],
   controllers: [MovieController],
   imports: [PrismaModule]
