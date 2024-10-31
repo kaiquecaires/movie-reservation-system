792f2f38b811b7344dbd271952e7753d78dfb6bc
kaiquecaires <caires.kaique@gmail.com>
2024-10-31 06:29:39 -0300

    feat: updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config'
 import { MovieModule } from './modules/movie/movie.module';
 import { SecurityModule } from './security/security.module';
+import { RoomModule } from './modules/room/room.module';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MovieModule,
-    SecurityModule
+    SecurityModule,
+    RoomModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
