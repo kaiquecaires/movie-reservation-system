822f47118b3859ad6ddb2f9a8c3cecfa3d494bb5
kaiquecaires <caires.kaique@gmail.com>
2024-11-04 08:00:28 -0300

    feat: added checkout dto

---
+++ src/modules/checkout/dto/body/checkout.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { ArrayMinSize, IsArray, IsInt, ValidateNested } from "class-validator";
+
+class ReservationDTO {
+  @ApiProperty({ example: 1, required: true })
+  @IsInt()
+  seatId: number
+}
+
+export class CheckoutDTO {
+  @ApiProperty({ example: 1, required: true })
+  @IsInt()
+  userId: number
+
+  @ApiProperty({ example: 1, required: true })
+  @IsInt()
+  showTimeId: number
+
+  @ApiProperty({ type: [ReservationDTO], required: true })
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => ReservationDTO)
+  reservations: ReservationDTO[]
+}
