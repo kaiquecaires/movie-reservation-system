330379dbfc3fce6e1fcc36310310bfd002f51dd6
kaiquecaires <caires.kaique@gmail.com>
2024-11-04 08:00:13 -0300

    feat: added show times repository

--- src/infra/postgresql/repositories/show-times.repository.ts
+++ src/infra/postgresql/repositories/show-times.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
 import { Prisma } from "@prisma/client";
+import { TotalPriceDTO } from "../dto/total-price.dto";
 
 @Injectable()
 export class ShowTimesRepository {
@@ -9,5 +10,20 @@ export class ShowTimesRepository {
   async create(data: Prisma.ShowTimeCreateArgs) {
     const response = await this.prisma.showTime.create(data)
     return response
+  }
+
+  async getTotalPrice(showTimeId: number, seatIdsList: number[]): Promise<TotalPriceDTO> {
+    const response = await this.prisma.$queryRaw<TotalPriceDTO[]>`
+    SELECT SUM(
+        CASE
+            WHEN s."seatType" = 'REGULAR' THEN st."priceRegular" 
+            ELSE st."priceVIP" 
+        END
+    ) AS total
+    FROM seats s
+    INNER JOIN show_times st ON st."roomId" = s."roomId" AND st.id = ${showTimeId}
+    WHERE s.id IN (${seatIdsList});
+    `
+    return response[0]
   }
 }
