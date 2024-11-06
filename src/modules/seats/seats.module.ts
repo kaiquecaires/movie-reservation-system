3c7cc22aa91b4901a56784b9b5a659e4817d9050
kaiquecaires <caires.kaique@gmail.com>
2024-11-06 06:00:40 -0300

    feat: added get available seats service service to module

--- src/modules/seats/seats.module.ts
+++ src/modules/seats/seats.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { CreateSeatsService } from "./services/create-seats.service";
 import { SeatsController } from "./seats.controller";
 import { PrismaModule } from "src/infra/postgresql/prisma.module";
+import { GetAvailableSeatsService } from "./services/get-available-seats.service";
 
 @Module({
   providers: [
-    CreateSeatsService
+    CreateSeatsService,
+    GetAvailableSeatsService
   ],
   controllers: [SeatsController],
   imports: [PrismaModule]
