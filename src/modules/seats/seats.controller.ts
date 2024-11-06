4e0cc2fe811eebe9b6c80aeed38f0505ca87b016
kaiquecaires <caires.kaique@gmail.com>
2024-11-06 06:00:13 -0300

    feat: added get available seats route

--- src/modules/seats/seats.controller.ts
+++ src/modules/seats/seats.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, HttpCode, Post, Res, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Post, Query, Res, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
-import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags, ApiOkResponse } from "@nestjs/swagger";
 import { CreateSeatsDTO } from "./dto/body/create-seats.dto";
 import { FastifyReply } from "fastify";
 import { CreateSeatsService } from "./services/create-seats.service";
 import { CreateSeatsResponseDTO } from "./dto/response/create-seats-response.dto";
+import { GetAvailableSeatsResponseDTO } from "./dto/response/get-available-seats-response.dto";
+import { GetAvailableSeatsDTO } from "./dto/query/get-available-seats.dto";
+import { GetAvailableSeatsService } from "./services/get-available-seats.service";
 
 @ApiTags('Seats | V1')
 @Controller({
@@ -13,7 +16,8 @@ import { CreateSeatsResponseDTO } from "./dto/response/create-seats-response.dto
 })
 export class SeatsController {
   constructor (
-    private readonly createSeatsService: CreateSeatsService
+    private readonly createSeatsService: CreateSeatsService,
+    private readonly getAvailableSeatsService: GetAvailableSeatsService
   ) {}
 
   @HttpCode(201)
@@ -30,5 +34,21 @@ export class SeatsController {
   async createMany(@Body() body: CreateSeatsDTO, @Res() res: FastifyReply) {
     const result = await this.createSeatsService.execute(body)
     return res.status(201).send(result)
+  }
+
+  @HttpCode(200)
+  @ApiOperation({
+    description: 'Route to get available seats'
+  })
+  @ApiOkResponse({
+    description: 'Result of this route',
+    type: GetAvailableSeatsResponseDTO
+  })
+  @ApiBearerAuth()
+  @Get('/available')
+  @UseGuards(AuthGuard('user'))
+  async getAvailableSeats(@Query() query: GetAvailableSeatsDTO) {
+    const response = await this.getAvailableSeatsService.execute(query.showTimeId)
+    return response
   }
 }
